ccb8270b0d4e31bdad65e5d897957f999c416dca
Matt <home1@Apples-MacBook-Pro.local>
2021-11-13 09:42:25 +0000

    second commit - refactor urgent checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -19,16 +19,6 @@ const App: React.FC = () => {
     allTasks: []
   })
 
-  // const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
-  //   setTaskState({
-  //     ...taskState,
-  //     currentTask: {
-  //       ...taskState.currentTask,
-  //       [e.target.name]: e.target.value
-  //     }
-  //   })
-  // }
-
   const onChangeHandler = <P extends keyof TaskInt>(prop: P, value: TaskInt[P]) => {
     setTaskState({
       ...taskState,
@@ -89,8 +79,6 @@ const App: React.FC = () => {
           name='name'
           value={taskState.currentTask.name}
           onChange={e => onChangeHandler('name', e.target.value)}
-          // value={taskState.currentTask.name}
-          // onChange={onChangeHandler}
         />
         <label htmlFor='task'>Description:</label>
         <input 
@@ -100,8 +88,6 @@ const App: React.FC = () => {
           name='task'
           value={taskState.currentTask.task}
           onChange={e => onChangeHandler('task', e.target.value)}
-          // value={taskState.currentTask.task}
-          // onChange={onChangeHandler}
         />
         <label htmlFor='task'>Urgent:</label>
         <input
@@ -119,102 +105,3 @@ const App: React.FC = () => {
 }
 
 export default App;
-
-
-
-// import React, { useState } from 'react';
-// import './App.css';
-// import Task, { TaskInt } from './components/Task';
-
-// const App: React.FC = () => {
-
-//   interface AllTasksInt {
-//     currentTask: TaskInt;
-//     allTasks: Array<TaskInt>
-//   }
-
-//   const [taskState, setTaskState] = useState<AllTasksInt>({
-//     currentTask: {
-//       name: '',
-//       task: '',
-//       deleteTask: () => {}
-//     },
-//     allTasks: []
-//   })
-
-//   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
-//     setTaskState({
-//       ...taskState,
-//       currentTask: {
-//         ...taskState.currentTask,
-//         [e.target.name]: e.target.value
-//       }
-//     })
-//   }
-
-//   const submitForm = (e: React.SyntheticEvent): void => {
-//     e.preventDefault();
-//     setTaskState({
-//       currentTask: {
-//         name: '',
-//         task: '',
-//         deleteTask: () => {}
-//       },
-//       allTasks: [
-//         ...taskState.allTasks,
-//         taskState.currentTask
-//       ]
-//     })
-//   }
-
-//   const deleteHandler = (index: number): void => {
-//     const filterUsers = taskState.allTasks.filter((task, i) => {
-//       return index !== i
-//     })
-//     setTaskState({
-//       ...taskState,
-//       allTasks: filterUsers
-//     })
-//   }
-
-//   const allTasks = taskState.allTasks.map((task, i) => {
-//     return (
-//       <Task 
-//         key={i} 
-//         name={task.name}
-//         task={task.task}
-//         deleteTask={() => deleteHandler(i)}
-//       />
-//     )
-//   })
-
-//   return (
-//     <div className="container">
-//       <h1>Todo List with React and Typescript</h1>
-//       <form onSubmit={submitForm} className="card">
-//         <label htmlFor='userName'>Task Name:</label>
-//         <input 
-//           id='userName'
-//           required
-//           type='text'
-//           name='name'
-//           value={taskState.currentTask.name}
-//           onChange={onChangeHandler}
-//         />
-//         <label htmlFor='task'>Description:</label>
-//         <input 
-//           id='task'
-//           required
-//           type='text'
-//           name='task'
-//           value={taskState.currentTask.task}
-//           onChange={onChangeHandler}
-//         />
-//         <button type='submit' className="submitBtn">Add Task</button>
-//       </form>
-//       {allTasks}
-//     </div>
-//   );
-// }
-
-// export default App;
